9ae23cbbb84a99dd31bce0d26b0e5b8aed437a24
Dominic Amwoza <doam2120@colorado.edu>
2025-08-12 14:55:00 -0600

    updated env.sh to match new environment for future c++ runs

--- tools/env.sh
+++ tools/env.sh
@@ -1,5 +1,15 @@
-#Shell script to load environment variables.
+#!/bin/bash
+# # Source the system default environment
+# source /opt/cray/pe/cpe/24.07/restore_lmod_system_defaults.sh
+
+ # Load required modules in the right order
 module restore
-module load nvhpc
+module load PrgEnv-nvidia
+module load cudatoolkit
+module load cray-mpich/8.1.25
+module load cray-fftw
+#
+# # Set PETSC path if needed
 export PETSC_PATH=/global/cfs/cdirs/mp118/software/petsc/install
-export LD_LIBRARY_PATH=$PETSC_PATH/lib:$LD_LIBRARY_PATH
+export MPIPATH=$CRAY_MPICH_DIR
+export LD_LIBRARY_PATH=/global/cfs/cdirs/mp118/software/petsc/install/lib:$LD_LIBRARY_PATH
