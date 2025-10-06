52bd7f2e338db2d5d342ef716e4f44ff71216756
Zhichen Feng <zhichen.feng@colorado.edu>
2025-10-06 09:59:59 -0700

    delete cpp files

--- src/fcnt.hpp
+++
@@ -1,5 +0,0 @@
-#include <stdlib.h>
-#pragma once
-
-
-double revers_c_(const int& num, const int& n);
--- tools/storeChanges.sh
+++
@@ -1,3 +0,0 @@
-#Store local code updates in run dir.
-REPO_ROOT=$(git rev-parse --show-toplevel)
-cp $REPO_ROOT/bin/codeChanges.txt .
